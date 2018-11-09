f585331a829db7af035c4335ce040a330ffabffa
D10 <D10@DESKTOP-7TVFJ24>
2018-11-09 19:57:36 +0100

    new

---
+++ precache-manifest.22c7a54193fc2956c905a318d8ba466d.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "5907726025809c3e275e",
+    "url": "/static/js/main.59077260.chunk.js"
+  },
+  {
+    "revision": "50319e393a7ddae3f452",
+    "url": "/static/js/1.50319e39.chunk.js"
+  },
+  {
+    "revision": "5907726025809c3e275e",
+    "url": "/static/css/main.cef42acc.chunk.css"
+  },
+  {
+    "revision": "f751c9a19f27ad82d0c24a3a38a6c1a4",
+    "url": "/index.html"
+  }
+];
