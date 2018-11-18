eb0782096fa008ff78e50a93364f66eb39b761c9
D10 <D10@DESKTOP-7TVFJ24>
2018-11-18 20:53:31 +0100

    Adding bootstrap template

--- _app/src/routes/routes.js
+++ _app/src/routes/routes.js
@@ -2,47 +2,55 @@ import { TodoApp } from "../components/todo/AppTodo";
 import { Topics } from "../components/topics/index";
 import { Home } from "../components/home/index";
 import { About } from "../components/about/index";
+import { SbAdmin } from "../components/dashboard";
 
 export const routes = [{
+    id: 0,
+    href: '/',
+    label: 'Home',
+    exact: true,
+    component: Home
+}, {
+    id: 1,
+    href: '/about',
+    label: 'About',
+    exact: false,
+    component: About
+}, {
+    id: 2,
+    href: '/topics',
+    label: 'Topics',
+    exact: false,
+    component: Topics,
+    children: [{
         id: 0,
-        href: '/',
-        label: 'Home',
-        exact: true,
-        component: Home
+        href: '/components',
+        label: 'Components',
+        exact: true
     }, {
         id: 1,
-        href: '/about',
-        label: 'About',
-        exact: false,
-        component: About
+        href: '/props-v-state',
+        label: 'Props v. State',
+        exact: true
     }, {
         id: 2,
-        href: '/topics',
-        label: 'Topics',
-        exact: false,
-        component: Topics,
-        children: [{
-            id: 0,
-            href: '/components',
-            label: 'Components',
-            exact: true
-        }, {
-            id: 1,
-            href: '/props-v-state',
-            label: 'Props v. State',
-            exact: true
-        }, {
-            id: 2,
-            href: '/counter',
-            label: 'Counter',
-            exact: true
-        }]
-    },
-    {
-        id: 3,
-        href: '/todolist',
-        label: 'Todo List',
-        exact: false,
-        component: TodoApp
-    }
+        href: '/counter',
+        label: 'Counter',
+        exact: true
+    }]
+},
+{
+    id: 3,
+    href: '/todolist',
+    label: 'Todo List',
+    exact: false,
+    component: TodoApp
+},
+{
+    id: 4,
+    href: '/dashboard',
+    label: 'Dashboard',
+    exact: false,
+    component: SbAdmin
+}
 ]
